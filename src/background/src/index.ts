c51fb6fd8ab688541e506a85eff5e0294fd0d4a1
Daisuke Yamamura <mucho613@gmail.com>
2021-11-27 01:40:59 +0900

    Regenerate API

--- src/background/src/index.ts
+++ src/background/src/index.ts
@@ -1,9 +1,9 @@
-import { Service, SignInRequestBody } from '../../api/index';
+import { Service, SignInRequestBody } from "./api";
 
 (async () => {
   const requestBody: SignInRequestBody = {
     email: 'mucho613@gmail.com',
-    password: '123456'
+    password: '123456',
   };
 
   const response = await Service.signIn(requestBody);
